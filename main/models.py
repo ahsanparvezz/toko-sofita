5632a88a608fb9ffa401d6f0a70fbb08477012b5
ahsanparvezz <ahsan.p1012@gmail.com>
2025-09-07 16:35:43 +0700

    menyelesaikan tugas 2, menyelesaikan main app

---
+++ main/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+    description = models.TextField()
+    thumbnail = models.URLField()
+    category = models.CharField(max_length=100)
+    is_featured = models.BooleanField(default=False)
+
+    # tambahan 
+    stock = models.IntegerField(default=0)
+    rating = models.FloatField(null=True, blank=True)
+    brand = models.CharField(max_length=100, blank=True)
+    quantity = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
