2d993a0076c3ee3a446937be7f40968b6d5ec181
ahsanparvezz <ahsan.p1012@gmail.com>
2025-09-12 18:39:01 +0700

    Penyelesaiain tugas 3 PBP pt 7

--- toko_sofita/settings.py
+++ toko_sofita/settings.py
@@ -34,7 +34,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "ahsan-parvez-tokosofita.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://ahsan-parvez-tokosofita.pbp.cs.ui.ac.id/"
+    "https://ahsan-parvez-tokosofita.pbp.cs.ui.ac.id",
 ]
 
 
