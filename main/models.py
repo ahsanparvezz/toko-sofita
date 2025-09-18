c92df4ab2f794bc2ee5d82e7d38fa2b24141953f
ahsanparvezz <ahsan.p1012@gmail.com>
2025-09-18 08:19:44 +0700

    Commit tugas ke 4

--- main/models.py
+++ main/models.py
@@ -1,14 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
     thumbnail = models.URLField()
     category = models.CharField(max_length=100)
     is_featured = models.BooleanField(default=False)
+
+
 
     # tambahan 
     stock = models.IntegerField(default=0)
@@ -18,6 +23,5 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
-    
 
 ## komentar tambahan trigger deploy
